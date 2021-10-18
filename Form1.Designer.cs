f6f2109e04ca9e1a2ff6ad4e446b802a86ba2f55
Tobias Lindberg <tobias.lindberg@ga.ntig.se>
2021-10-18 09:11:56 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace Övn2._2_Personalregisterh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRegistreraFörsäljning = new System.Windows.Forms.Button();
+            this.tbxFörsäljning = new System.Windows.Forms.TextBox();
+            this.tbxProvision = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxNamn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbxTimlön = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbxNamnKonsult = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tbxMånadslön = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbxNamnSäljare = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lbxRegister = new System.Windows.Forms.ListBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lbxLöneutbetalningar = new System.Windows.Forms.ListBox();
+            this.tbxTotalLönekostnad = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnBeräknaLöner = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRegistreraFörsäljning);
+            this.groupBox1.Controls.Add(this.tbxFörsäljning);
+            this.groupBox1.Controls.Add(this.tbxProvision);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbxNamn);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(192, 154);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Säljare";
+            // 
+            // btnRegistreraFörsäljning
+            // 
+            this.btnRegistreraFörsäljning.Location = new System.Drawing.Point(9, 120);
+            this.btnRegistreraFörsäljning.Name = "btnRegistreraFörsäljning";
+            this.btnRegistreraFörsäljning.Size = new System.Drawing.Size(172, 23);
+            this.btnRegistreraFörsäljning.TabIndex = 1;
+            this.btnRegistreraFörsäljning.Text = "Registrera försäljning";
+            this.btnRegistreraFörsäljning.UseVisualStyleBackColor = true;
+            // 
+            // tbxFörsäljning
+            // 
+            this.tbxFörsäljning.Location = new System.Drawing.Point(81, 85);
+            this.tbxFörsäljning.Name = "tbxFörsäljning";
+            this.tbxFörsäljning.Size = new System.Drawing.Size(100, 20);
+            this.tbxFörsäljning.TabIndex = 5;
+            // 
+            // tbxProvision
+            // 
+            this.tbxProvision.Location = new System.Drawing.Point(81, 53);
+            this.tbxProvision.Name = "tbxProvision";
+            this.tbxProvision.Size = new System.Drawing.Size(100, 20);
+            this.tbxProvision.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Provision";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Försäljning";
+            // 
+            // tbxNamn
+            // 
+            this.tbxNamn.Location = new System.Drawing.Point(81, 19);
+            this.tbxNamn.Name = "tbxNamn";
+            this.tbxNamn.Size = new System.Drawing.Size(100, 20);
+            this.tbxNamn.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Namn";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.tbxTimlön);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.tbxNamnKonsult);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(221, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(192, 154);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Konsult";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Registrera arbetspass";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // tbxTimlön
+            // 
+            this.tbxTimlön.Location = new System.Drawing.Point(81, 53);
+            this.tbxTimlön.Name = "tbxTimlön";
+            this.tbxTimlön.Size = new System.Drawing.Size(100, 20);
+            this.tbxTimlön.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Timlön";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Arbetad tid";
+            // 
+            // tbxNamnKonsult
+            // 
+            this.tbxNamnKonsult.Location = new System.Drawing.Point(81, 19);
+            this.tbxNamnKonsult.Name = "tbxNamnKonsult";
+            this.tbxNamnKonsult.Size = new System.Drawing.Size(100, 20);
+            this.tbxNamnKonsult.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Namn";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.tbxMånadslön);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.tbxNamnSäljare);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Location = new System.Drawing.Point(431, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(192, 154);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Säljare";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Registrera månadslön";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // tbxMånadslön
+            // 
+            this.tbxMånadslön.Location = new System.Drawing.Point(81, 53);
+            this.tbxMånadslön.Name = "tbxMånadslön";
+            this.tbxMånadslön.Size = new System.Drawing.Size(100, 20);
+            this.tbxMånadslön.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 56);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Månadslön";
+            // 
+            // tbxNamnSäljare
+            // 
+            this.tbxNamnSäljare.Location = new System.Drawing.Point(81, 19);
+            this.tbxNamnSäljare.Name = "tbxNamnSäljare";
+            this.tbxNamnSäljare.Size = new System.Drawing.Size(100, 20);
+            this.tbxNamnSäljare.TabIndex = 2;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 22);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Namn";
+            // 
+            // lbxRegister
+            // 
+            this.lbxRegister.FormattingEnabled = true;
+            this.lbxRegister.Location = new System.Drawing.Point(12, 197);
+            this.lbxRegister.Name = "lbxRegister";
+            this.lbxRegister.Size = new System.Drawing.Size(193, 108);
+            this.lbxRegister.TabIndex = 3;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(70, 181);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(62, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "REGISTER";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(257, 181);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(127, 13);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "LÖNEUTBETALNINGAR";
+            // 
+            // lbxLöneutbetalningar
+            // 
+            this.lbxLöneutbetalningar.FormattingEnabled = true;
+            this.lbxLöneutbetalningar.Location = new System.Drawing.Point(221, 197);
+            this.lbxLöneutbetalningar.Name = "lbxLöneutbetalningar";
+            this.lbxLöneutbetalningar.Size = new System.Drawing.Size(193, 108);
+            this.lbxLöneutbetalningar.TabIndex = 5;
+            // 
+            // tbxTotalLönekostnad
+            // 
+            this.tbxTotalLönekostnad.Location = new System.Drawing.Point(440, 255);
+            this.tbxTotalLönekostnad.Name = "tbxTotalLönekostnad";
+            this.tbxTotalLönekostnad.Size = new System.Drawing.Size(172, 20);
+            this.tbxTotalLönekostnad.TabIndex = 8;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(437, 239);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(61, 9);
+            this.label11.TabIndex = 7;
+            this.label11.Text = "Total lönekostnad";
+            // 
+            // btnBeräknaLöner
+            // 
+            this.btnBeräknaLöner.Location = new System.Drawing.Point(440, 197);
+            this.btnBeräknaLöner.Name = "btnBeräknaLöner";
+            this.btnBeräknaLöner.Size = new System.Drawing.Size(172, 23);
+            this.btnBeräknaLöner.TabIndex = 9;
+            this.btnBeräknaLöner.Text = "Beräkna löner";
+            this.btnBeräknaLöner.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 322);
+            this.Controls.Add(this.btnBeräknaLöner);
+            this.Controls.Add(this.tbxTotalLönekostnad);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lbxLöneutbetalningar);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lbxRegister);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbxNamn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRegistreraFörsäljning;
+        private System.Windows.Forms.TextBox tbxFörsäljning;
+        private System.Windows.Forms.TextBox tbxProvision;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbxTimlön;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbxNamnKonsult;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbxMånadslön;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbxNamnSäljare;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ListBox lbxRegister;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListBox lbxLöneutbetalningar;
+        private System.Windows.Forms.TextBox tbxTotalLönekostnad;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnBeräknaLöner;
+    }
+}
+
